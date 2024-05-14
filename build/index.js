fad72bd58b1955baf3e7c3573d49fcc7de2333cc
Martín Frangul <77142482+martinfrangul@users.noreply.github.com>
2024-05-14 11:44:56 +0200

    Terminado comportamiento de votos cuando click. Mejorar el sistema para eliminar un voto

--- build/index.js
+++ build/index.js
@@ -10,16 +10,26 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 let getNextJoke = document.getElementById('nexJoke');
 let jokeData;
+const icanHadDad = 'https://icanhazdadjoke.com/';
+const chuckNorris = 'https://api.chucknorris.io/jokes/random';
 function isEmptyObject(obj) {
     return Object.keys(obj).length === 0;
 }
+let jokeUrl;
 function getJoke() {
     return __awaiter(this, void 0, void 0, function* () {
+        let randomNumber = Math.floor(Math.random() * (2 - 1 + 1) + 1);
+        if (randomNumber == 1) {
+            jokeUrl = icanHadDad;
+        }
+        else {
+            jokeUrl = chuckNorris;
+        }
         if (isEmptyObject(voteObject) === false) {
             reportJokesListener();
         }
         try {
-            const response = yield fetch('https://icanhazdadjoke.com/', {
+            const response = yield fetch(jokeUrl, {
                 method: 'GET',
                 headers: {
                     Accept: 'application/json',
@@ -27,10 +37,18 @@ function getJoke() {
             });
             const data = yield response.json();
             let jokeText = document.getElementById('joke');
-            jokeText.innerText = data.joke;
-            jokeData = data;
-            voteObject = {};
-            console.log(reportJokes);
+            if (jokeUrl == icanHadDad) {
+                jokeText.innerText = data.joke;
+                jokeData = data;
+                voteObject = {};
+                console.log(reportJokes);
+            }
+            if (jokeUrl == chuckNorris) {
+                jokeText.innerText = data.value;
+                jokeData = data;
+                voteObject = {};
+                console.log(reportJokes);
+            }
             return jokeData;
         }
         catch (error) {
@@ -41,32 +59,72 @@ function getJoke() {
 const reportJokesListener = () => {
     reportJokes.push(voteObject);
 };
-// VOTOS
+//////// VOTACIONES //////////
 const reportJokes = [];
 let voteObject = {};
 const voteListener = (id) => {
     const date = new Date();
+    let voteIcon1 = document.getElementById('vote-1');
+    let voteIcon2 = document.getElementById('vote-2');
+    let voteIcon3 = document.getElementById('vote-3');
     if (id === 1) {
-        voteObject = {
-            joke: jokeData.joke,
-            score: 1,
-            date: date.toISOString(),
-        };
+        voteIcon1.classList.add('vote-icon-1-clicked');
+        voteIcon2.classList.remove('vote-icon-2-clicked');
+        voteIcon3.classList.remove('vote-icon-3-clicked');
+        if (jokeUrl == icanHadDad) {
+            voteObject = {
+                joke: jokeData.joke,
+                score: 1,
+                date: date.toISOString(),
+            };
+        }
+        if (jokeUrl == chuckNorris) {
+            voteObject = {
+                joke: jokeData.value,
+                score: 1,
+                date: date.toISOString(),
+            };
+        }
     }
     if (id === 2) {
-        voteObject = {
-            joke: jokeData.joke,
-            score: 2,
-            date: date.toISOString(),
-        };
+        voteIcon2.classList.add('vote-icon-2-clicked');
+        voteIcon1.classList.remove('vote-icon-1-clicked');
+        voteIcon3.classList.remove('vote-icon-3-clicked');
+        if (jokeUrl == icanHadDad) {
+            voteObject = {
+                joke: jokeData.joke,
+                score: 2,
+                date: date.toISOString(),
+            };
+        }
+        if (jokeUrl == chuckNorris) {
+            voteObject = {
+                joke: jokeData.value,
+                score: 2,
+                date: date.toISOString(),
+            };
+        }
     }
     if (id === 3) {
-        voteObject = {
-            joke: jokeData.joke,
-            score: 3,
-            date: date.toISOString(),
-        };
+        voteIcon3.classList.add('vote-icon-3-clicked');
+        voteIcon1.classList.remove('vote-icon-1-clicked');
+        voteIcon2.classList.remove('vote-icon-2-clicked');
+        if (jokeUrl == icanHadDad) {
+            voteObject = {
+                joke: jokeData.joke,
+                score: 3,
+                date: date.toISOString(),
+            };
+        }
+        if (jokeUrl == chuckNorris) {
+            voteObject = {
+                joke: jokeData.value,
+                score: 3,
+                date: date.toISOString(),
+            };
+        }
     }
 };
+////////////////////////// 
 getNextJoke.addEventListener('click', getJoke);
 window.addEventListener('load', getJoke);
--- index.ts
+++ index.ts
@@ -2,18 +2,27 @@ let getNextJoke = document.getElementById('nexJoke');
 
 let jokeData: object;
 
+const icanHadDad = 'https://icanhazdadjoke.com/';
+const chuckNorris = 'https://api.chucknorris.io/jokes/random';
+
 function isEmptyObject(obj: object) {
   return Object.keys(obj).length === 0;
 }
+let jokeUrl: string;
 
 async function getJoke() {
-
+  let randomNumber = Math.floor(Math.random() * (2 - 1 + 1) + 1);
+  if (randomNumber == 1) {
+    jokeUrl = icanHadDad;
+  } else {
+    jokeUrl = chuckNorris;
+  }
   if (isEmptyObject(voteObject) === false) {
     reportJokesListener();
   }
 
   try {
-    const response = await fetch('https://icanhazdadjoke.com/', {
+    const response = await fetch(jokeUrl, {
       method: 'GET',
       headers: {
         Accept: 'application/json',
@@ -22,10 +31,18 @@ async function getJoke() {
 
     const data = await response.json();
     let jokeText = document.getElementById('joke');
-    jokeText.innerText = data.joke;
-    jokeData = data;
-    voteObject = {};
-    console.log(reportJokes);
+    if (jokeUrl == icanHadDad) {
+      jokeText.innerText = data.joke;
+      jokeData = data;
+      voteObject = {};
+      console.log(reportJokes);
+    }
+    if (jokeUrl == chuckNorris) {
+      jokeText.innerText = data.value;
+      jokeData = data;
+      voteObject = {};
+      console.log(reportJokes);
+    }
 
     return jokeData;
   } catch (error) {
@@ -36,37 +53,84 @@ const reportJokesListener = () => {
   reportJokes.push(voteObject);
 };
 
-// VOTOS
+//////// VOTACIONES //////////
 
 const reportJokes = [];
 
 let voteObject: object = {};
 
-const voteListener = (id : number) => {
+
+
+const voteListener = (id: number) => {
   const date = new Date();
 
+  let voteIcon1 = document.getElementById('vote-1')
+  let voteIcon2 = document.getElementById('vote-2')
+  let voteIcon3 = document.getElementById('vote-3')
+  
   if (id === 1) {
-    voteObject = {
-      joke: jokeData.joke,
-      score: 1,
-      date: date.toISOString(),
-    };
+    voteIcon1.classList.add('vote-icon-1-clicked')
+    voteIcon2.classList.remove('vote-icon-2-clicked')
+    voteIcon3.classList.remove('vote-icon-3-clicked')
+
+    if (jokeUrl == icanHadDad) {
+      voteObject = {
+        joke: jokeData.joke,
+        score: 1,
+        date: date.toISOString(),
+      };
+    }
+    if (jokeUrl == chuckNorris) {
+      voteObject = {
+        joke: jokeData.value,
+        score: 1,
+        date: date.toISOString(),
+      };
+    }
   }
   if (id === 2) {
-    voteObject = {
-      joke: jokeData.joke,
-      score: 2,
-      date: date.toISOString(),
-    };
+    voteIcon2.classList.add('vote-icon-2-clicked')
+    voteIcon1.classList.remove ('vote-icon-1-clicked')
+    voteIcon3.classList.remove ('vote-icon-3-clicked')
+
+    if (jokeUrl == icanHadDad) {
+      voteObject = {
+        joke: jokeData.joke,
+        score: 2,
+        date: date.toISOString(),
+      };
+    }
+    if (jokeUrl == chuckNorris) {
+      voteObject = {
+        joke: jokeData.value,
+        score: 2,
+        date: date.toISOString(),
+      };
+    }
   }
   if (id === 3) {
-    voteObject = {
-      joke: jokeData.joke,
-      score: 3,
-      date: date.toISOString(),
-    };
+    voteIcon3.classList.add ('vote-icon-3-clicked')
+    voteIcon1.classList.remove ('vote-icon-1-clicked')
+    voteIcon2.classList.remove ('vote-icon-2-clicked')
+    
+    if (jokeUrl == icanHadDad) {
+      voteObject = {
+        joke: jokeData.joke,
+        score: 3,
+        date: date.toISOString(),
+      };
+    }
+    if (jokeUrl == chuckNorris) {
+      voteObject = {
+        joke: jokeData.value,
+        score: 3,
+        date: date.toISOString(),
+      };
+    }
   }
 };
+
+////////////////////////// 
 
 getNextJoke.addEventListener('click', getJoke);
 window.addEventListener('load', getJoke);
